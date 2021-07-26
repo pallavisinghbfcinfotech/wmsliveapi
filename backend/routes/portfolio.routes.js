37bf04a4beffc94063d81abfa4dc837259bdc17d
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-07-26 14:06:33 +0530

    Update portfolio.routes.js

--- backend/routes/portfolio.routes.js
+++ backend/routes/portfolio.routes.js
@@ -1,7 +1,6 @@
 module.exports = app => {
     const port_folio = require("../controllers/port_folio.controller.js");
-    const port_foliotest = require("../controllers/port_folio.controllertest.js");
-  
+   
     app.post("/port_folio_data", port_folio.port_folio_data);
     
    // app.post("/port_folio_camsdata", port_folio.port_folio_camsdata);
