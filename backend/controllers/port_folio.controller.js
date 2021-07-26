b64e6fd474f13e2b54a41efc26a79facb1abfd99
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-07-26 13:35:53 +0530

    Update port_folio.controller.js

--- backend/controllers/port_folio.controller.js
+++ backend/controllers/port_folio.controller.js
@@ -13,6 +13,8 @@ const Axios = require("axios");
 var operationsCompleted = 0; let dataarr = []; var user = []; let datascheme = [];
 var doc = ""; var lastarray = []; var newarray = [];
 
+module.exports = port_folio_data;
+
 exports.port_folio_data = (req, res) => {
     try {
         var resdata = "";
