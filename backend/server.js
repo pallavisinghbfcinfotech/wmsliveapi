5addde8b7a71a74588ce9f1d8360c5c003546dfb
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-07-27 10:55:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import bodyParser from 'body-parser';
 import nodemailer from 'nodemailer';
 import localStorage from 'localStorage'
 import Axios from 'axios'
- import moment from 'moment';
- import  port_folio_data  from './routes/portfolio.routes.js';
+import moment from 'moment';
+import { port_folio_data } from './routes/portfolio.routes.js';
 var Schema = mongoose.Schema;
 dotenv.config();
 
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
 // require("./routes/portfolio.routes.js")(app)
 //import portfolio from "./routes/portfolio.routes.js";
   
-  //  app.post("/port_folio", port_folio_data.port_folio_data);
+    app.post("/port_folio", port_folio_data.port_folio_data);
 //app.post("./routes/portfolio.routes.js");
 
 
