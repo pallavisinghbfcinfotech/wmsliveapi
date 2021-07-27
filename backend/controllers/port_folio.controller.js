8d9c5d89fd068d245b9f9f09b49798c4021e7e84
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-07-27 10:26:43 +0530

    Update port_folio.controller.js

--- backend/controllers/port_folio.controller.js
+++ backend/controllers/port_folio.controller.js
@@ -25,7 +25,7 @@ var doc = ""; var lastarray = []; var newarray = [];
 export default {
 }
 
- class port_folio_data = (req, res) => {
+ class port_folio_data(req, res) => {
 //     try {
 //         var resdata = "";
 //         pipeline1 = [  //trans_cams
