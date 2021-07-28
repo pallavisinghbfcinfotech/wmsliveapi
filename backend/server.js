6672f82ba1f94d483212b12ff3c4b11bdd5ced5d
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-07-28 17:13:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ mongoose.connect("mongodb+srv://developer:developer@cluster0.jxhzj.mongodb.net/w
    //else{ console.log('Connected to ' + db, ' + ', response); }
    else{ console.log('Connected to ' + db); }
 });
-
+mongoose.Promise = global.Promise;
  console.log("I am mongodb", db);
 mongoose.connect(mongodbUrl, {
 	useNewUrlParser:true,
